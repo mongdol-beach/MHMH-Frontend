5435d8d633c285d366cfa0f5b3beae9596675382
wo-o29 <woogur29@gmail.com>
2024-12-07 06:17:31 +0900

    refactor: 컬러 토큰 수정

--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -1,14 +1,16 @@
 export const MOBILE_MEDIA_QUERY = `@media screen and (max-width: 768px)`;
 
 export const COLORS = {
-  "--card-color-blue": "#5881F3",
-  "--card-color-beige": "#E5E5E3",
-  "--card-color-yellow": "#FFDC79",
-  "--card-color-red": "#FF5255",
-  "--card-color-pink": "#FFAEC5",
-  "-grayscale-800": "#1E1E1E",
+  "--card-color-blue": "#5881f3",
+  "--card-color-sky_b": "#97b2ff",
+  "--card-color-beige": "#f9f4e3",
+  "--card-color-yellow": "#ffdc79",
+  "--card-color-red": "#ff5255",
+  "--card-color-pink": "#ffaec5",
+  "-grayscale-800": "#1e1e1e",
   "-grayscale-600": "#606060",
   "-grayscale-400": "#939393",
-  "-grayscale-200": "#C7C7C7",
-  "-grayscale-100": "#E8E8E8",
+  "-grayscale-200": "#c7c7c7",
+  "-grayscale-100": "#e8e8e8",
+  "-grayscale-50": "#f7f9fe",
 };

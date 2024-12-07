fc2aaf0a5da520293a50aecb02781c5da45d1364
Donghoon <thdehdgns@gmail.com>
2024-12-07 16:42:12 +0900

    feat: Home 아이콘 클릭하면 메인 페이지로 돌아가기

--- src/components/Situation/Header/index.tsx
+++ src/components/Situation/Header/index.tsx
@@ -1,13 +1,17 @@
 import * as S from "./styled";
 import IconArrow from "@assets/icons/icon-arrow.svg";
 import IconHome from "@assets/icons/icon-home.svg";
+import { Link } from "react-router-dom";
+import PAGE_PATH from "../../../constants/path";
 
 const Header = () => {
   return (
     <S.Container>
       <S.Icon src={IconArrow} />
       <S.Title>상황별 토픽</S.Title>
-      <S.Icon src={IconHome} />
+      <Link to={PAGE_PATH.MAIN}>
+        <S.Icon src={IconHome} />
+      </Link>
     </S.Container>
   );
 };

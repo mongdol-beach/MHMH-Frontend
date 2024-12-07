e61e26858312413bb155d9a36f572b084259554f
Donghoon <thdehdgns@gmail.com>
2024-12-07 16:31:06 +0900

    feat: description 영역 추가

--- src/components/Situation/index.tsx
+++ src/components/Situation/index.tsx
@@ -5,7 +5,15 @@ const Situation = () => {
   return (
     <>
       <Header />
-      <S.Main>메인</S.Main>
+      <S.Main>
+        <S.DescriptionBox>
+          <S.Description>
+            토픽 추천이 필요한
+            <br />
+            상황을 선택 해주세요
+          </S.Description>
+        </S.DescriptionBox>
+      </S.Main>
     </>
   );
 };
--- src/components/Situation/styled.ts
+++ src/components/Situation/styled.ts
@@ -1,3 +1,17 @@
 import styled from "styled-components";
 
 export const Main = styled.main``;
+
+export const DescriptionBox = styled.div`
+  margin-top: 3.5rem;
+  margin-bottom: 3rem;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Description = styled.p`
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 150%; /* 1.5rem */
+  letter-spacing: -0.02rem;
+`;

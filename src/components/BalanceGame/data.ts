a0654112597b89d4e2b83cc4012bdd4b9d648983
DHyeonJ <eogus724@naver.com>
2024-12-08 09:03:33 +0900

    feat: 밸런스게임 페이지 추가

---
+++ src/components/BalanceGame/data.ts
@@ -0,0 +1,27 @@
+export const BALANCEGAME = [
+  {
+    subject: "지금 제주도행 비행기를 탄다면, 둘 중 더 최악인 상황을 골라보자",
+    questionA: "내가 찼던 전 애인과 같이 있기",
+    questionB: "나를 찼던 전 애인과 같이 있기",
+  },
+  {
+    subject: "회사에서 더 최악인 사람",
+    questionA: "일 잘하지만 성격 개판",
+    questionB: "일 못하지만 성격 좋음",
+  },
+  {
+    subject: "회사에서 더 최악인 사람2",
+    questionA: "일 잘하지만 성격 개판",
+    questionB: "일 못하지만 성격 좋음",
+  },
+  {
+    subject: "회사에서 더 최악인 사람3",
+    questionA: "일 잘하지만 성격 개판",
+    questionB: "일 못하지만 성격 좋음",
+  },
+  {
+    subject: "회사에서 더 최악인 사람4",
+    questionA: "일 잘하지만 성격 개판",
+    questionB: "일 못하지만 성격 좋음",
+  },
+];

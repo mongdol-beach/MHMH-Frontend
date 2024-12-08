09b9edd3decda0ba9197f800ed687c79da0c37d2
Donghoon <thdehdgns@gmail.com>
2024-12-08 15:24:45 +0900

    feat: 랜덤 토픽 페이지에 TopicCards 추가

--- src/components/RandomTopics/index.tsx
+++ src/components/RandomTopics/index.tsx
@@ -1,11 +1,17 @@
 import Header from "../Header";
+import TopicCards from "../TopicCards";
+import { RANDOM_TOPICS } from "./data";
 import * as S from "./styled";
 
 const RandomTopics = () => {
   return (
     <>
       <Header title="랜덤 토픽 추천" />
-      <S.Main></S.Main>
+      <S.Main>
+        <S.TopicCardsContainer>
+          <TopicCards topics={RANDOM_TOPICS} />
+        </S.TopicCardsContainer>
+      </S.Main>
     </>
   );
 };

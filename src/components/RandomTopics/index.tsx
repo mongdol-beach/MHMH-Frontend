d61d514bcc418fa0528052515c13ab5ff44c8ef5
Donghoon <thdehdgns@gmail.com>
2024-12-08 14:56:25 +0900

    feat: 랜덤 토픽 페이지 추가

---
+++ src/components/RandomTopics/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../Header";
+import * as S from "./styled";
+
+const RandomTopics = () => {
+  return (
+    <>
+      <Header title="랜덤 토픽 추천" />
+      <S.Main></S.Main>
+    </>
+  );
+};
+
+export default RandomTopics;

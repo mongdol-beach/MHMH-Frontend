795ec8f95974d442bd05f05c1f1273c9c0776b3a
Donghoon <thdehdgns@gmail.com>
2024-12-08 16:44:14 +0900

    refactor: useRandomTopics 타입 정리

--- src/hooks/getRandomTopics.ts
+++ src/hooks/getRandomTopics.ts
@@ -2,8 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { instance } from "../apis/axios";
 import { Topic } from "../types/topic";
 
+interface RandomTopicsResponse {
+  topics: Topic[];
+}
+
 export const useRandomTopics = () => {
-  return useQuery<{ topics: Topic[] }>({
+  return useQuery<RandomTopicsResponse, Error>({
     queryKey: ["randomTopics"],
     queryFn: async () => await instance.get("/topic").then((res) => res.data),
   });

3b8898792d162727e4f268c414d9294ed8fe2866
정민규 <jmg2165@naver.com>
2025-01-06 23:09:06 +0900

    feat. 토픽완료 카카오 공유하기 기능 구현

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,33 @@
 declare module "swiper/css";
+
+declare global {
+  interface Window {
+    Kakao: {
+      init(apiKey: string): void;
+      isInitialized(): boolean;
+      Link: {
+        sendDefault(options: {
+          objectType: string;
+          content: {
+            title: string;
+            description: string;
+            imageUrl: string;
+            link: {
+              mobileWebUrl: string;
+              webUrl: string;
+            };
+          };
+          buttons: Array<{
+            title: string;
+            link: {
+              mobileWebUrl: string;
+              webUrl: string;
+            };
+          }>;
+        }): void;
+      };
+    };
+  }
+}
+
+export {};
--- src/components/SituationFinish/index.tsx
+++ src/components/SituationFinish/index.tsx
@@ -9,6 +9,8 @@ import Restart from "@assets/icons/re-start.svg";
 import KaKao from "../../assets/icons/kakao_small.svg";
 import Share from "../../assets/icons/share.svg";
 
+export {}; // global 선언을 위해 필요
+
 interface FinishProps {
   topics: {
     id: number;
@@ -35,6 +37,35 @@ const Finish: React.FC<FinishProps> = ({ topics }) => {
     } catch (error) {
       console.error("링크 복사 실패:", error);
     }
+  };
+
+  const handleKakaoShare = () => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init("def5476a1875ffa04852c1ecd8950592");
+    }
+
+    window.Kakao.Link.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "말해머해",
+        description: "대화 주제 추천 서비스, 말해머해를 체험해보세요!",
+        imageUrl:
+          "https://brick-william-6f5.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fe321b4cb-8569-4a87-9b86-2845eb22f8d7%2Faa2b6f56-0316-4c2d-9674-3f3d013ef9f5%2Fog-image.png?table=block&id=1564e173-3f21-807f-90c2-f226ffed128b&spaceId=e321b4cb-8569-4a87-9b86-2845eb22f8d7&width=1420&userId=&cache=v2",
+        link: {
+          mobileWebUrl: "https://mh-mh.vercel.app/",
+          webUrl: "https://mh-mh.vercel.app/",
+        },
+      },
+      buttons: [
+        {
+          title: "웹으로 보기",
+          link: {
+            mobileWebUrl: "https://mh-mh.vercel.app/",
+            webUrl: "https://mh-mh.vercel.app/",
+          },
+        },
+      ],
+    });
   };
 
   return (
@@ -67,7 +98,7 @@ const Finish: React.FC<FinishProps> = ({ topics }) => {
           <br />
           친구에게도 <S.FooterStrong>“말해머해”</S.FooterStrong>를 알려주세요
           <S.ShareContainer>
-            <S.ShareImage src={KaKao} />
+            <S.ShareImage src={KaKao} onClick={handleKakaoShare} />
             <S.ShareImage src={Share} onClick={handleShare} />
           </S.ShareContainer>
         </S.FooterP>
